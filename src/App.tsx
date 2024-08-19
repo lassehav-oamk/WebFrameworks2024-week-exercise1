c5b2acdbe3314e1a423bd007f2132cd67f439802
Lasse Haverinen <lasse.haverinen@gmail.com>
2024-08-19 15:08:25 +0300

    Initial version ready

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {  
+  return (
+    <>
+      <div>
+        This is week exercise 1. Read README.md for instructions.
+      </div>
+    </>
+  )
+}
+
+export default App
